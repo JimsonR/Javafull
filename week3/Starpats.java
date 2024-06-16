b31ff53302b85779b8f9afe79091316322bc047e
JimsonR <jimsonjimmy2008@gmail.com>
2024-06-16 08:02:59 +0530

    commit

--- week3/Starpats.java
+++ week3/Starpats.java
@@ -2,9 +2,25 @@ public class Starpats {
     public static void main(String[] args) {
         for (int x = 0; x < 10; x++) {
             for(int y = 0; y < 10-x; y++) {
-                System.out.print("*");
+                System.out.print(" ");
+
+            }
+            for(int y = 0; y <= x; y++) {
+                System.out.print(" "+"*");
             }
             System.out.println();
+
+        }
+        for (int x = 0; x < 10; x++) {
+            for(int y = 0; y <= x; y++) {
+                System.out.print(" ");
+
+            }
+            for(int y = 0; y < 10-x; y++) {
+                System.out.print(" "+"*");
+            }
+            System.out.println();
+
         }
     }
 }
