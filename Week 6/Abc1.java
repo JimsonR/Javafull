c047850e67af9312443028b8c837e0b2005ade03
JimsonR <jimsonjimmy2008@gmail.com>
2024-07-04 17:44:30 +0530

    commit

---
+++ Week 6/Abc1.java
@@ -0,0 +1,24 @@
+
+class Test1{
+//     static void m1(){
+//        System.out.println("sad");
+//
+//    }
+//    private void m2(){
+//        System.out.println("ASd");
+//    }
+
+}
+
+public class Abc1 extends Test1{
+    void m2(){
+
+    }
+    public static void main(String[] args){
+        Test1 abc = new Abc1();
+//        abc.m2();
+        Integer[] arr = new Integer[10];
+        System.out.println(arr[1]);
+
+    }
+ }
